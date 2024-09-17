8f6030c25a9891f11a240e9aa8955138541e60e4
Matheus <mgustavocoder@gmail.com>
2024-09-17 10:35:14 -0300

    remove credentials from code.

--- src/common/config/constants.ts
+++ src/common/config/constants.ts
@@ -3,6 +3,6 @@ export const USER_INFO_ENDPOINT = 'https://pyy8x7fi75.execute-api.us-east-1.amaz
 //Database
 export const DB_PORT: number = 3306
 export const DB_NAME: string = 'lanchonete'
-export const DB_USER: string = 'admin'
-export const DB_PASSWORD: string = 'mysql-postech-fiap'
-export const DB_HOST: string = 'postech-fiap.cluster-clvalyvrbinl.us-east-1.rds.amazonaws.com'
+export const DB_USER: string = process.env.DB_USER || ''
+export const DB_PASSWORD: string = process.env.DB_PASSWORD || ''
+export const DB_HOST: string = process.env.DB_HOST || ''
